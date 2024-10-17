8b71b5778b3ff3c12c47b0caf96aa82a2700e22d
YanzOffcWeb <clockcraftid7654@gmail.com>
2024-10-17 23:01:47 +0700

    Update ai4chat.js

--- ai/ai4chat.js
+++ ai/ai4chat.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
 
 
     async function bossMode(prompt) {
-        const url = new URL("https://yw85opafq6.execute-api.us-east-1.amazonaws.com/default/boss_mode_15aug");
+        const url = new URL(`https://yw85opafq6.execute-api.us-east-1.amazonaws.com/default/boss_mode_15aug`);
         url.search = new URLSearchParams({
             text: prompt,
             country: "Europe",
