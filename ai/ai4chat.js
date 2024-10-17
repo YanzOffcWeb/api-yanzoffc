7ac4d08099d2f09ed490fcb0ef2494720c1cbfb1
YanzOffcWeb <clockcraftid7654@gmail.com>
2024-10-17 23:03:10 +0700

    Update ai4chat.js

--- ai/ai4chat.js
+++ ai/ai4chat.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
 
 
     async function bossMode(prompt) {
-        const url = new URL(`https://yw85opafq6.execute-api.us-east-1.amazonaws.com/default/boss_mode_15aug`);
+        const url = new URL('https://yw85opafq6.execute-api.us-east-1.amazonaws.com/default/boss_mode_15aug');
         url.search = new URLSearchParams({
             text: prompt,
             country: "Europe",
@@ -15,7 +15,7 @@ module.exports = function(app) {
             const response = await axios.get(url.toString(), {
                 headers: {
                     "User-Agent": "Mozilla/5.0 (Linux; Android 11; Infinix) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.0.0 Mobile Safari/537.36",
-                    Referer: `https://www.ai4chat.co/pages/riddle-generator`
+                    Referer: "https://www.ai4chat.co/pages/riddle-generator"
                 }
             });
 
